891a1ec59169e0492b6d999bb6d7acb118eb1405
Ty <Tylergarrett2003@gmail.com>
2026-09-03 04:06:01 -0400

    Implement database connection and table creation
    Added SQLite database connection and table creation functionality.

--- database/database.py
+++ database/database.py
@@ -1,1 +1,43 @@
 
+import sqlite3
+
+DB_NAME = "detections.db"
+
+def get_connection():
+    return sqlite3.connect(DB_NAME)
+
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS detections (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            filename TEXT NOT NULL,
+            species TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+# this is for testing if the table is corretly made
+```
+#def test_database():
+#    conn = get_connection()
+ #   cursor = conn.cursor()
+
+ #   cursor.execute(""" INSERT INTO detections (filename, species) VALUES (?,?)""",("dolphin001.wav", "Dolphin"))
+##   conn.commit()
+
+#    cursor.execute("SELECT * FROM detections")
+ #   results = cursor.fetchall()
+
+ #   print(results)
+
+#    conn.close()
+
+if __name__ == "__main__":
+    create_tables()
+    test_database()
+  
