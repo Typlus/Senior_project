c9e320ee590a90824764a70953e1050ac58145c0
Ty <Tylergarrett2003@gmail.com>
2026-09-13 18:02:15 -0400

    Implement Streamlit UI for species filtering and pagination
    Added Streamlit interface for filtering species and setting confidence levels. Implemented pagination for displaying results in a DataFrame.

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
+import streamlit as st
+import pandas as pd
+import math
+import functions 
+st.title ("ImmersaVLM")
+# grab what species of animal they want to filter from the database
+    # this can be done by either A: going through and outputing allt he unique species in the table or B have a running dictionary of them and pull from that
+user_selected_s = st.multiselect("species to filter by :",[ "dogs","cats","fish","cars"])
+user_selected_c = st.slider ("what is minumum confidance level:", min_value=00, max_value=100 , value=0 , step=10, format="%d%%")
+
+# call filter function if any of this was changed 
+
+
+
+# create talbe header
+st.subheader("table")
+
+# create varible for all results, this varible can change later to when filter is added and allows for differnt changes
+results = functions.get_all_results()
+
+tf = pd.DataFrame(
+    results,
+    columns=["ID","Species","Filename"]
+)
+# set up for generate reports function later on. #TODO ADD FUNCTIONALTY TO HAVE IT CREATE A PDF WITH ALL THE EXPECTED QUALITES
+tf["Generate report"] = "Generate Report"
+
+# limits the page to 15 entries per page
+ROWS_PER_PAGE = 15
+total_pages = max(1, (len(tf) + ROWS_PER_PAGE - 1) // ROWS_PER_PAGE) #
+
+# 2. Render the interactive pagination buttons FIRST (on top)
+current_page = st.pagination(num_pages=total_pages)
+
+# 3. Slice the DataFrame based on the active button page
+start_idx = (current_page - 1) * ROWS_PER_PAGE
+end_idx = start_idx + ROWS_PER_PAGE
+sliced_tf = tf.iloc[start_idx:end_idx]
+
+st.dataframe(sliced_tf, use_container_width=True, hide_index=True, height=565)
+
+
 
