1db7efc3d041b2e657fb44a2db58b462ce1e5554
Ty <Tylergarrett2003@gmail.com>
2026-09-22 16:06:06 -0400

    Implement cached connection for Supabase client
    Refactor Supabase client initialization to use caching.

--- database.py
+++ database.py
@@ -3,8 +3,12 @@
 import streamlit as st
 from supabase import create_client
 
-supabase = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
-)
+@st.cache_resource
+def init_connection():
+    return create_client(
+        st.secrets["SUPABASE_URL"],
+        st.secrets["SUPABASE_KEY"]
+    )
+
+supabase = init_connection()
 
