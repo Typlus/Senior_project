b9f873a83089dcf2a6aab28244f482356badcc1b
Ty <Tylergarrett2003@gmail.com>
2026-09-22 15:32:02 -0400

    Migrate database connection from SQLite to Supabase
    Replaced SQLite with Supabase for database management.

--- database/database.py
+++ database/database.py
@@ -1,43 +1,10 @@
 
-import sqlite3
 
-DB_NAME = "detections.db"
+import streamlit as st
+from supabase import create_client
 
-def get_connection():
-    return sqlite3.connect(DB_NAME)
+supabase = create_client(
+    st.secrets["SUPABASE_URL"],
+    st.secrets["SUPABASE_KEY"]
+)
 
-
-def create_tables():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS detections (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            species TEXT,
-            filename TEXT NOT NULL,
-            Confidence integer
-            
-        )
-    """)
-    conn.commit()
-    conn.close()
-
-# this is for testing if the table is corretly made
-
-#def test_database():
-#    conn = get_connection()
- #   cursor = conn.cursor()
-
- #   cursor.execute(""" INSERT INTO detections (filename, species) VALUES (?,?)""",("dolphin001.wav", "Dolphin"))
-##   conn.commit()
-
-#    cursor.execute("SELECT * FROM detections")
- #   results = cursor.fetchall()
-
- #   print(results)
-
-#    conn.close()
-
-if __name__ == "__main__":
-    create_tables()
-  
